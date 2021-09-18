37e461e49044855c2d658550bef8b42570d105aa
yhlcontadores <yhlcontadores@gmail.com>
2021-09-18 18:20:45 -0500

    P

--- enviar.php
+++ enviar.php
@@ -2,7 +2,6 @@
 <?php
 $name = $_POST['name'];
 $mail = $_POST['mail'];
-$phone = $_POST['phone'];
 $message = $_POST['message'];
 
 $header = 'From: ' . $mail . " \r\n";
@@ -12,15 +11,14 @@ $header .= "Content-Type: text/plain";
 
 $message = "Este mensaje fue enviado por: " . $name . " \r\n";
 $message .= "Su e-mail es: " . $mail . " \r\n";
-$message .= "Teléfono de contacto: " . $phone . " \r\n";
+
 $message .= "Mensaje: " . $_POST['message'] . " \r\n";
 $message .= "Enviado el: " . date('d/m/Y', time());
 
 $para = 'yhlcontadores@gmail.com';
-$asunto = 'Mensaje de... (Escribe como quieres que se vea el remitente de tu correo)';
+$asunto = 'CONSULTA';
 
 mail($para, $asunto, utf8_decode($message), $header);
 
 header("Location:index.html");
 ?>
--->
