4a60f115ae32381f0311f36b89d5f40874e3fe42
yhlcontadores <yhlcontadores@gmail.com>
2021-09-19 09:19:10 -0500

    ajuste de form

--- enviar.php
+++ enviar.php
@@ -1,38 +1,23 @@
 <?php
-if (!isset($_POST['email'])) {
+error_reporting(0);
+$nombre = $_POST['nombre'];
+$email= $_POST['email'];
+$opinion=$_POST['opinion'];
+$mensaje=$_POST['mensaje'];
+
+$header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
+$header .= "Mime-Version: 1.0 \r\n";
+$header .= "Content-Type: text/plain";
+
+$mensaje = "Este mensaje fue enviado por " . $nombre . " \r\n";
+$mensaje .= "Su e-mail es: " . $mail . " \r\n";
+$mensaje .= "mensaje " . $_POST['mensaje'] . " \r\n";
+$mensaje .= "Enviado el " . date('d/m/Y', time());
+
+$para = yhlcontadores@gmail.com;
+$asunto = 'Consulta';
+
+mail($para, $asunto, utf8_decode($mensaje), $header);
+header("position:constant.html")
+echo 'mensaje enviado correctamente';
 ?>
-  <form action="<?=$_SERVER['PHP_SELF']?>" method="post">
-    <label>
-      Nombre:
-      <input name="nombre" type="text" />
-    </label>
-    <label>
-      Teléfono:
-      <input name="telefono" type="text" />
-    </label>
-    <label>
-      Email:
-      <input name="email" type="text" />
-    </label>
-    <label>
-      Mensaje:
-      <textarea name="mensaje" rows="6" cols="50"></textarea>
-    </label>
-    <input type="reset" value="Borrar" />
-    <input type="submit" value="Enviar" />
-  </form>
-<?php
-}else{
-  $mensaje="Mensaje del formulario de contacto";
-  $mensaje.= "\nNombre: ". $_POST['nombre'];
-  $mensaje.= "\nEmail: ".$_POST['email'];
-  $mensaje.= "\nTelefono: ". $_POST['telefono'];
-  $mensaje.= "\nMensaje: \n".$_POST['mensaje'];
-  $destino= "yhlcontadores@gmail.com";
-  $remitente = $_POST['email'];
-  $asunto = "Mensaje enviado por: ".$_POST['nombre'];
-  mail($destino,$asunto,$mensaje,"FROM: $remitente");
-?>
-  <p><strong>Mensaje enviado.</strong></p>
-<?php
-}?>
