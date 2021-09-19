45f832de3e3221495a00c2276b4b823c4f6abf25
yhlcontadores <yhlcontadores@gmail.com>
2021-09-19 12:14:47 -0500

    incorporacion src PHP

--- enviar.php
+++ enviar.php
@@ -1,4 +1,11 @@
 <?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require 'src/Exception.php';
+require 'src/PHPMailer.php';
+require 'src/SMTP.php';
+
 $nombre = $_POST['nombre'];
 $mail = $_POST['email'];
 $mensaje = $_POST['mensaje'];
