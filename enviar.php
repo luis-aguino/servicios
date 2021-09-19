f998c9999228cde49b31b501f96ba85f7f4300bd
yhlcontadores <yhlcontadores@gmail.com>
2021-09-19 10:49:07 -0500

    mo

--- enviar.php
+++ enviar.php
@@ -1,61 +1,44 @@
 <?php
+//Import PHPMailer classes into the global namespace
+//These must be at the top of your script, not inside a function
 use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
 
-require 'PHPMailer/src/Exception.php';
-require 'PHPMailer/src/PHPMailer.php';
-require 'PHPMailer/src/SMTP.php';
- 
+//Load Composer's autoloader
+require 'vendor/autoload.php';
+
+//Create an instance; passing `true` enables exceptions
 $mail = new PHPMailer(true);
+
 try {
+    //Server settings
+    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+    $mail->isSMTP();                                            //Send using SMTP
+    $mail->Host       = 'smtp.example.com';                     //Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+    $mail->Username   = 'user@example.com';                     //SMTP username
+    $mail->Password   = 'secret';                               //SMTP password
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+    $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
-    $mail->SMTPOptions = array(
-        'ssl' => array(
-        'verify_peer' => false,
-        'verify_peer_name' => false,
-        'allow_self_signed' => true
-        )
-    );
+    //Recipients
+    $mail->setFrom('yhlcontadores@gmail.com', 'yhlcontadores');
+       $mail->addReplyTo("yhlcontadores@gmail.com', 'yhlcontadores");
+    $mail->addCC('yhlcontadores@gmail.com');
+    
 
-    $mail->SMTPDebug = 2;  // Sacar esta línea para no mostrar salida debug
-    $mail->isSMTP();
-    $mail->Host = 'smtp.gmail.com';  // Host de conexión SMTP
-    $mail->SMTPAuth = true;
-    $mail->Username = 'yhlcontadores@gmail.com';                 // Usuario SMTP
-    $mail->Password = '';                           // Password SMTP
-    $mail->SMTPSecure = 'tls';                            // Activar seguridad TLS
-    $mail->Port = 465;                                    // Puerto SMTP
+    //Attachments
+    $mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
+    $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
 
-    $mail->SMTPOptions = ['ssl'=> ['allow_self_signed' => true]];  // Descomentar si el servidor SMTP tiene un certificado autofirmado
-    $mail->SMTPSecure = false;				// Descomentar si se requiere desactivar cifrado (se suele usar en conjunto con la siguiente línea)
-    $mail->SMTPAutoTLS = false;			// Descomentar si se requiere desactivar completamente TLS (sin cifrado)
- 
-    $mail->setFrom('yhlcontadores@gmail.com');		// Mail del remitente
-    $mail->addAddress('luasjaramillo759@gmail.com');     // Mail del destinatario
- 
-    $mail->isHTML(true);
-    $mail->Subject = 'Ref; Consulta';  // Asunto del mensaje
-    $mail->Body    = 'Gracias por contatarnos <b>YHLCONTADORES</b>';    // Contenido del mensaje (acepta HTML)
-    $mail->AltBody = 'Estudiaremos la consulta y nos comunicaremos a la brevedad del caso.';    // Contenido del mensaje alternativo (texto plano)
- error_reporting(0);
-$nombre = $_POST['nombre'];
-$email= $_POST['email'];
-$opinion=$_POST['opinion'];
-$mensaje=$_POST['mensaje'];
+    //Content
+    $mail->isHTML(true);                                  //Set email format to HTML
+    $mail->Subject = 'Here is the subject';
+    $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
 
-$header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
-$header .= "Mime-Version: 1.0 \r\n";
-$header .= "Content-Type: text/plain";
-
-$mensaje = "Este mensaje fue enviado por " . $nombre . " \r\n";
-$mensaje .= "Su e-mail es: " . $mail . " \r\n";
-$mensaje .= "mensaje " . $_POST['mensaje'] . " \r\n";
-$mensaje .= "Enviado el " . date('d/m/Y', time());
-
-$para = yhlcontadores@gmail.com;
-$asunto = 'Consulta';
-
-mail($para, $asunto, utf8_decode($mensaje), $header);
-header("position:constant.html")
-echo 'mensaje enviado correctamente';
-?>
+    $mail->send();
+    echo 'Message has been sent';
+} catch (Exception $e) {
+echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}"; ?>
