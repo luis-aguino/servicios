9ba7c74a47b5ce09aae1a6a6cc6247209417d87d
yhlcontadores <yhlcontadores@gmail.com>
2021-09-19 09:53:33 -0500

    incorporacion de archivos miller

--- enviar.php
+++ enviar.php
@@ -1,4 +1,43 @@
 <?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require 'PHPMailer/src/Exception.php';
+require 'PHPMailer/src/PHPMailer.php';
+require 'PHPMailer/src/SMTP.php';
+ 
+$mail = new PHPMailer(true);
+try {
+    $mail->SMTPDebug = 2;  // Sacar esta línea para no mostrar salida debug
+    $mail->isSMTP();
+    $mail->Host = 'mail.midominio.com';  // Host de conexión SMTP
+    $mail->SMTPAuth = true;
+    $mail->Username = 'remitente@midominio.com';                 // Usuario SMTP
+    $mail->Password = 'MiPa33W0rD';                           // Password SMTP
+    $mail->SMTPSecure = 'tls';                            // Activar seguridad TLS
+    $mail->Port = 587;                                    // Puerto SMTP
+
+    #$mail->SMTPOptions = ['ssl'=> ['allow_self_signed' => true]];  // Descomentar si el servidor SMTP tiene un certificado autofirmado
+    #$mail->SMTPSecure = false;				// Descomentar si se requiere desactivar cifrado (se suele usar en conjunto con la siguiente línea)
+    #$mail->SMTPAutoTLS = false;			// Descomentar si se requiere desactivar completamente TLS (sin cifrado)
+ 
+    $mail->setFrom('remitente@midominio.com');		// Mail del remitente
+    $mail->addAddress('destinatario@gmail.com');     // Mail del destinatario
+ 
+    $mail->isHTML(true);
+    $mail->Subject = 'Contacto desde formulario';  // Asunto del mensaje
+    $mail->Body    = 'Este es el contenido del mensaje <b>en negrita!</b>';    // Contenido del mensaje (acepta HTML)
+    $mail->AltBody = 'Este es el contenido del mensaje en texto plano';    // Contenido del mensaje alternativo (texto plano)
+ 
+    $mail->send();
+    echo 'El mensaje ha sido enviado';
+} catch (Exception $e) {
+    echo 'El mensaje no se ha podido enviar, error: ', $mail->ErrorInfo;
+}
+
+
+
+<?php
 error_reporting(0);
 $nombre = $_POST['nombre'];
 $email= $_POST['email'];
